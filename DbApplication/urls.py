0cca4eb86bb1ae50b2e16bd4642f7291865bac76
GOPALA KRISHNA V <120028677+Gopala05@users.noreply.github.com>
2024-01-27 15:44:18 +0530

    API Endpoint Update

--- DbApplication/urls.py
+++ DbApplication/urls.py
@@ -10,15 +10,15 @@ from .views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/create_user/', UserSignup.as_view(), name='create_user'),
-    path('api/sign_in/', UserSignInAPIView.as_view(), name='sign_in'),
-    path('api/get_user/', GetUserAPIView.as_view(), name='get_user'),
-    path('api/places/', AdventurePlaceListAPIView.as_view(), name='adventure_place_list'),
-    path('api/place/<int:id>/', AdventurePlaceDetailAPIView.as_view(), name='adventure_place_detail'),
-    path('api/customer-details/', CustomerDetailCreateAPIView.as_view(), name='customer_detail_create'),
-    path('api/place/<int:id>/packages/', AdventurePackageDetailAPIView.as_view(), name='adventure_package_detail'),
-    path('api/booking-details/', BookingDetailCreateAPIView.as_view(), name='booking_detail_create'),
-    path('api/feedback/', FeedbackCreateAPIView.as_view(), name='feedback_create'),
-    path('api/logout/', LogoutAPIView.as_view(), name='logout'),
-    re_path("user/", views.UserSignup.as_view())
+    path('api/create_user', UserSignup.as_view(), name='create_user'),
+    path('api/sign_in', UserSignInAPIView.as_view(), name='sign_in'),
+    path('api/get_user', GetUserAPIView.as_view(), name='get_user'),
+    path('api/places', AdventurePlaceListAPIView.as_view(), name='adventure_place_list'),
+    path('api/place', AdventurePlaceDetailAPIView.as_view(), name='adventure_place_detail'),
+    path('api/customer-details', CustomerDetailCreateAPIView.as_view(), name='customer_detail_create'),
+    path('api/place/<int:id>/packages', AdventurePackageDetailAPIView.as_view(), name='adventure_package_detail'),
+    path('api/booking-details', BookingDetailCreateAPIView.as_view(), name='booking_detail_create'),
+    path('api/feedback', FeedbackCreateAPIView.as_view(), name='feedback_create'),
+    path('api/logout', LogoutAPIView.as_view(), name='logout'),
+    re_path("user", views.UserSignup.as_view())
 ]
